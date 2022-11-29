e7d60eda7caafafc7f868dc3bc9971f901d923cc
Jeroen van der Heijden <jeroen@cesbit.com>
2022-11-29 12:20:27 +0100

    Upd dns

---
+++ lib/check/dns.py
@@ -0,0 +1,112 @@
+import logging
+from typing import Optional
+from libprobe.asset import Asset
+import asyncio
+import copy
+import dns.asyncresolver
+import dns.message
+from dns.resolver import NoAnswer
+from libprobe.exceptions import (
+    CheckException,
+    IgnoreCheckException,
+    IncompleteResultException,
+)
+
+
+def get_item(measured_time: float, answers: int, item: dict) -> dict:
+    item['answers'] = answers
+    item['measuredTime'] = measured_time
+    return item
+
+
+async def dns_single(loop, aresolver, qname, qtype, item) -> Optional[dict]:
+    start = loop.time()
+    try:
+        response = (await aresolver.resolve(qname, qtype)).response
+    except NoAnswer:
+        return None
+    except Exception as e:
+        msg = str(e) or type(e).__name__
+        raise CheckException(msg)  # this might result in a incomplete result
+    else:
+        record = response.answer[0].to_rdataset()[0]
+        item[qtype] = str(record)
+        return get_item(loop.time() - start, 1, item)
+
+
+async def dns_multi(aresolver, qname, qtype, item) -> int:
+    try:
+        response = (await aresolver.resolve(qname, qtype)).response
+    except NoAnswer:
+        return 0
+    except Exception as e:
+        msg = str(e) or type(e).__name__
+        raise CheckException(msg)  # this might result in a incomplete result
+    else:
+        records = response.answer[0].to_rdataset()
+        item[qtype] = sorted(map(str, records))
+        return 1
+
+
+async def dns_query(qname: str, name_server: str):
+    aresolver = dns.asyncresolver.Resolver()
+    try:
+        aresolver.nameservers = [name_server]
+    except ValueError as e:
+        raise CheckException(str(e))
+
+    loop = asyncio.get_event_loop()
+    item = {
+        'name': name_server,
+        'query': qname,
+    }
+
+    if (await dns_single(loop, aresolver, qname, 'CNAME', item)):
+        return item
+    if (await dns_single(loop, aresolver, qname, 'PTR', item)):
+        return item
+
+    answers = 0
+    start = loop.time()
+    answers += (await dns_multi(aresolver, qname, 'A', item))
+    answers += (await dns_multi(aresolver, qname, 'AAAA', item))
+    answers += (await dns_multi(aresolver, qname, 'CAA', item))
+    answers += (await dns_multi(aresolver, qname, 'DS', item))
+    answers += (await dns_multi(aresolver, qname, 'MX', item))
+    answers += (await dns_multi(aresolver, qname, 'NS', item))
+    answers += (await dns_multi(aresolver, qname, 'SOA', item))
+    answers += (await dns_multi(aresolver, qname, 'SRV', item))
+    answers += (await dns_multi(aresolver, qname, 'TXT', item))
+    return get_item(loop.time() - start, answers, item)
+
+
+async def check_dns(
+        asset: Asset,
+        asset_config: dict,
+        check_config: dict) -> dict:
+    name_servers = check_config.get('nameServers')
+    if not name_servers or not isinstance(name_servers, (tuple, list)):
+        logging.warning(
+            f'Check did not run; {field} is not provided, invalid or empty')
+        raise IgnoreCheckException
+
+    query = check_config.get('fqdn')
+    if not query:
+        query = asset.name
+
+    items = []
+    result = {'dns': items}
+    check_exc: Optional[CheckException] = None
+    for name_server in name_servers:
+        try:
+            item = await dns_query(query, name_server)
+        except CheckException as e:
+            check_exc = e
+        else:
+            items.append(item)
+
+    if check_exc is None:
+        return result
+    elif items:
+        raise IncompleteResultException(str(check_exc), result)
+    raise check_exc
---
+++ test_dns.py
@@ -0,0 +1,82 @@
+import asyncio
+import unittest
+from lib.check.dns import check_dns
+from libprobe.exceptions import (
+    IncompleteResultException,
+    CheckException,
+)
+
+
+def _run(fqdn=None, name_servers=['8.8.8.8']):
+    check_config = {
+        'fqdn': fqdn,
+        'nameServers': name_servers
+    }
+    result = asyncio.run(check_dns(None, None, check_config))
+    return result['dns']
+
+
+class TestProbe(unittest.TestCase):
+
+    def test_check_dns_cname(self):
+        res = _run('www.cesbit.com')
+        self.assertIn('CNAME', res[0])
+        self.assertNotIn('A', res[0])
+        self.assertEqual(1, res[0]['answers'])
+
+    def test_check_dns_ptr(self):
+        res = _run('4.4.8.8.in-addr.arpa')
+        self.assertIn('PTR', res[0])
+        self.assertNotIn('A', res[0])
+        self.assertEqual(1, res[0]['answers'])
+
+    def test_check_dns_more(self):
+        res = _run('cesbit.com')
+        self.assertNotIn('PTR', res[0])
+        self.assertIn('A', res[0])
+        self.assertGreater(res[0]['answers'], 5)
+
+    def test_check_dns_incomplete(self):
+        try:
+            _run('www.cesbit.com', name_servers=['0.0', '8.8.8.8'])
+        except IncompleteResultException as e:
+            result = e.result
+            self.assertIn('CNAME', result['dns'][0])
+        else:
+            raise Exception('IncompleteResultException not raised')
+
+    def test_check_dns_multi(self):
+        res = _run('www.cesbit.com', name_servers=['8.8.4.4', '8.8.8.8'])
+        self.assertEqual(len(res), 2)
+        self.assertEqual(res[0]['name'], '8.8.4.4')
+        self.assertEqual(res[1]['name'], '8.8.8.8')
+        self.assertEqual(1, res[0]['answers'])
+        self.assertEqual(1, res[1]['answers'])
+
+    def test_check_dns_incomplete(self):
+        try:
+            _run('www.cesbit.com', name_servers=['0.0', '8.8.8.8'])
+        except IncompleteResultException as e:
+            result = e.result
+            self.assertIn('CNAME', result['dns'][0])
+            self.assertRegex(str(e), r'nameserver 0.0 is not an IP address')
+        else:
+            raise Exception('IncompleteResultException not raised')
+
+    def test_check_dns_check_exc(self):
+        with self.assertRaisesRegex(
+                CheckException,
+                r'The resolution lifetime expired after'):
+            _run('www.cesbit.com', name_servers=['8.8.8.0'])
+
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(TestProbe('test_check_dns_cname'))
+    suite.addTest(TestProbe('test_check_dns_ptr'))
+    suite.addTest(TestProbe('test_check_dns_more'))
+    suite.addTest(TestProbe('test_check_dns_incomplete'))
+    suite.addTest(TestProbe('test_check_dns_multi'))
+    suite.addTest(TestProbe('test_check_dns_check_exc'))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
